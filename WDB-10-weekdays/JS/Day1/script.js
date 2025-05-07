cf6e294f1862fd68bed9607861526648609ee354
mtcodingtech <mtcodingtech@gmail.com>
2025-05-07 16:08:50 +0630

    day1 js and challenge

---
+++ WDB-10-weekdays/JS/Day1/script.js
@@ -0,0 +1,33 @@
+// var username = "John";
+// username = "Rose";
+// console.log(username)
+
+// let age = 20;
+// age = 30;
+// console.log(age)
+
+// const address = "YGN";
+// address = "Mdy";
+// console.log(address)
+
+// **************************
+
+// let user = "John";
+// let admin = user;
+
+// console.log(admin)
+
+// **************************
+
+// var username = "John";
+
+// if (true) {
+//   let username = "Rose";
+//   console.log(username);
+// }
+// console.log(username);
+// **************************
+
+"use strict";
+let num = 5;
+console.log(num);
