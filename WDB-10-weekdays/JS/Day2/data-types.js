bccdd1a986cc115b965f642c0fa38bdbf90225df
mtcodingtech <mtcodingtech@gmail.com>
2025-05-09 16:10:11 +0630

    day2 js

---
+++ WDB-10-weekdays/JS/Day2/data-types.js
@@ -0,0 +1,45 @@
+// let age = 20.2;
+// let result = "not a number" / 2;
+// console.log(age, typeof age)
+// console.log(result, typeof result)
+
+// ******************************
+// let user1 = "John";
+// let user2 = 'Rose';
+// let user3 = `MT`;
+// console.log(typeof user3)
+
+// ******************************
+
+// let isGreater = 5 < 1;
+// console.log(isGreater, typeof isGreater)
+
+// ******************************
+
+// let user1 = null;
+// // user1 = "John";
+// console.log(user1, typeof user1) // object
+
+// let user2;
+// console.log(user2, typeof user2)
+
+// console.log(user1 == user2) // value
+// console.log(user1 === user2) // value and data type
+
+// ******************************
+
+// let fruits = ["apple", "orange", "mango"];  //index
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3]) // undefined
+// console.log(typeof fruits) // object
+
+// ******************************
+
+let user = {
+  name: "John",
+  age: 20,
+};
+console.log(user.name, user.age)
+console.log(user["name"], user["age"])
