ced34856948070f7051653eae766bd62412e1fd9
mtcodingtech <mtcodingtech@gmail.com>
2025-05-14 15:00:41 +0630

    day3 js

---
+++ WDB-10-weekdays/JS/Day3/condition.js
@@ -0,0 +1,33 @@
+let fruit = "Orange";
+
+// if (fruit === "Apple") {
+//   console.log("Apple");
+// } else if (fruit === "Orange") {
+//   console.log("Orange");
+// } else {
+//   console.log("Other fruit");
+// }
+
+// let result = fruit === "Apple"
+//     ? console.log("Apple")
+//     : fruit === "Orange"
+//     ? console.log("Orange")
+//     : "Other Fruit";
+
+// console.log(result)
+
+// *******************************
+
+let day = 2;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  default:
+    console.log("Other day");
+    break;
+}
---
+++ WDB-10-weekdays/JS/Day3/operators.js
@@ -0,0 +1,66 @@
+// let num1 = 10;
+// let num2 = 2;
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 ** num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+// num1++;
+// num1++;
+// num1--;
+// console.log(num1)
+
+// **************************************
+
+// let num1 = 10;
+// num1 += 20; // 10 + 20
+// console.log(num1)
+
+//  **************************************
+// let msg = "Hello";
+// let username = "John";
+// // let result = msg += username;
+// msg += " ";
+// msg += username;
+// console.log(msg);
+
+// *******************************
+
+// let msg = "Hello";
+// let username = "John";
+// // let result = msg + " " + username;
+// let result = `${msg} ${username}, how are you?`
+// console.log(result)
+
+// *******************************
+// console.log(10 + "20")
+
+// console.log(10 == "10") // true
+// console.log(10 === "10") // false
+
+// console.log(10 !== "10")
+
+// **************************
+
+// let age = 18;
+// let result = age >= 18 ? "adult" : "child";
+// console.log(result)
+
+// **************************
+
+let username = "John";
+
+console.log(username === "John");
+console.log(typeof username === "string");
+
+// let result = username === "John" && typeof username === "string";
+
+// let result = username === "aJohn" || typeof username === "string";
+
+// let result = !(10 == 12)
+
+let result = username === "aJohn" || "HELLO" && "HI";
+
+console.log("result", result);
