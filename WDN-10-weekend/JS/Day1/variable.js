b3a7562a787ec0ec733e548d55085a3443e1f0de
mtcodingtech <mtcodingtech@gmail.com>
2025-06-09 21:55:26 +0630

    js

---
+++ WDN-10-weekend/JS/Day1/variable.js
@@ -0,0 +1,27 @@
+// var username = "John";
+// username = "Rose";
+// console.log(username)
+
+// let age = 20;
+// age = 25;
+// console.log(age)
+
+// const city = "Ygn";
+// city = "mdy";
+// console.log(city)
+
+// *******************
+
+// var username = "John";
+
+// if(true){
+//     let username = "Rose";
+//     console.log(username)
+// }
+
+// console.log(username)
+
+// **********************
+"use strict";
+let num = 5;
+console.log(num)
